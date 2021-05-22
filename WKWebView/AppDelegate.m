d5952cd77be3fe2148950d3bd6bc9b4110c820c2
lian <lian@liandeMacBook-Pro.local>
2021-05-22 11:26:46 +0800

    test

--- WKWebView/AppDelegate.m
+++ WKWebView/AppDelegate.m
@@ -13,7 +13,9 @@
 @end
 
 @implementation AppDelegate
-
+- (void) test {
+    
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
